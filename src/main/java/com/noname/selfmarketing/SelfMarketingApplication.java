f88cba08d75f1229cb4101d029ac99bf1520c4ef
Andrzej Karolczak <qfs.andrzej.karolczak@gmail.com>
2020-02-20 20:18:45 +0100

    First

---
+++ src/main/java/com/noname/selfmarketing/SelfMarketingApplication.java
@@ -0,0 +1,13 @@
+package com.noname.selfmarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfMarketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noname/selfmarketing/SelfMarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noname.selfmarketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfMarketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
